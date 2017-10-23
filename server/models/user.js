4bf497cd96d7c51ca96aa6cd1883f72c37791d6f
Fred Magnin <frederic.magnin@gmail.com>
2017-10-23 17:46:02 +0200

    login email

--- server/models/user.js
+++ server/models/user.js
@@ -3,12 +3,12 @@ const passportLocalMongoose = require("passport-local-mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  // the username and password are added by passportLocalMongoose
-  // we can add any extra information like facebooktokens (or
-  // others) as well as a picture, etc
-  name: String
+    // the username and password are added by passportLocalMongoose
+    // we can add any extra information like facebooktokens (or
+    // others) as well as a picture, etc
+    username: String
 });
 
-userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(passportLocalMongoose, { usernameField: "email" });
 
 module.exports = mongoose.model("User", userSchema);
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,73 +5,75 @@ const User = require("../models/user");
 const config = require("../config");
 
 router.post("/signup", (req, res, next) => {
-  // extract the info we need from the body
-  // of the request
-  const { username, name, password } = req.body;
+    // extract the info we need from the body
+    // of the request
 
-  // create the new user
-  // notice how we don't pass the password because
-  // we're letting User.register add the hashed version
-  // for us
-  const user = new User({
-    username,
-    name
-  });
+    const { username, email, password } = req.body;
 
-  User.register(user, password, err => {
-    if (err) {
-      return next(err);
-    }
-    res.json({ success: true });
-  });
+    // create the new user
+    // notice how we don't pass the password because
+    // we're letting User.register add the hashed version
+    // for us
+    const user = new User({
+        username,
+        email
+    });
+
+    User.register(user, password, err => {
+        if (err) {
+            return next(err);
+        }
+
+        res.json({ success: true });
+    });
 });
 
 // User.authenticate() returns a function
 const authenticate = User.authenticate();
 router.post("/login", (req, res, next) => {
-  const { username, password } = req.body;
-  // check if we have a username and password
-  if (username && password) {
-    // test if the credentials are valid
-    authenticate(username, password, (err, user, failed) => {
-      if (err) {
-        // an unexpected error from the database
-        return next(err);
-      }
-      if (failed) {
-        // failed logging (bad password, too many attempts, etc)
-        return res.status(401).json({
-          error: failed.message
+    const { email, password } = req.body;
+    // check if we have a username and password
+    if (email && password) {
+        // test if the credentials are valid
+        authenticate(email, password, (err, user, failed) => {
+            if (err) {
+                // an unexpected error from the database
+                return next(err);
+            }
+            if (failed) {
+                // failed logging (bad password, too many attempts, etc)
+                return res.status(401).json({
+                    error: failed.message
+                });
+            }
+            if (user) {
+                // success!! Save the user id
+                // NEVER save the password here
+                // the id is usually enough because we can get back
+                // the actual user by fetching the database later
+                const payload = {
+                    id: user.id
+                };
+                // generate a token and send it
+                // this token will contain the user.id encrypted
+                // only the server is able to decrypt it
+                // for the client, this is just a token, he knows that
+                // he has to send it
+                const token = jwt.encode(payload, config.jwtSecret);
+                res.json({
+                    user: {
+                        username: user.username,
+                        email: user.email,
+                        _id: user._id
+                    },
+                    token
+                });
+            }
         });
-      }
-      if (user) {
-        // success!! Save the user id
-        // NEVER save the password here
-        // the id is usually enough because we can get back
-        // the actual user by fetching the database later
-        const payload = {
-          id: user.id
-        };
-        // generate a token and send it
-        // this token will contain the user.id encrypted
-        // only the server is able to decrypt it
-        // for the client, this is just a token, he knows that
-        // he has to send it
-        const token = jwt.encode(payload, config.jwtSecret);
-        res.json({
-          user: {
-            name: user.name,
-            username: user.username,
-            _id: user._id
-          },
-          token
-        });
-      }
-    });
-  } else {
-    // unauthorized error
-    res.sendStatus(401);
-  }
+    } else {
+        // unauthorized error
+        res.sendStatus(401);
+    }
 });
 
 module.exports = router;
