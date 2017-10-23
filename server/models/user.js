dc4a213703db0ba1992bdd0f19c137a72f5177b8
Nicolas <nicolas.barthez@gmail.com>
2017-10-23 18:05:31 +0200

    Change app backend in server

--- server/models/user.js
+++ server/models/user.js
@@ -2,12 +2,49 @@ const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     // the username and password are added by passportLocalMongoose
     // we can add any extra information like facebooktokens (or
     // others) as well as a picture, etc
-    username: String
-});
+    username: String,
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user"
+    },
+    picProfile: {
+      type: String,
+      default: "/images/avatar.png"
+    },
+    location: {
+      type: String,
+      default: "****"
+    },
+    bio: {
+      type: String,
+      default: "none"
+    },
+    following: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    score: {
+      type: Number,
+      default: 0
+    },
+    watchList: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "WatchItem"
+      }
+    ],
+    nbFollower: {
+      type: Number,
+      default: 0
+    }
+  },
+  {
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
+  }
+);
 
 userSchema.plugin(passportLocalMongoose, { usernameField: "email" });
 
