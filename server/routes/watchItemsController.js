4f13b6caae1632b10977ec8c3c229ae86c459493
Nicolas <nicolas.barthez@gmail.com>
2017-10-26 23:39:45 +0200

    current insights

--- server/routes/watchItemsController.js
+++ server/routes/watchItemsController.js
@@ -4,10 +4,36 @@ const Stock = require("../models/stock");
 const User = require("../models/user");
 const Babble = require("../models/babble");
 const WatchItem = require("../models/watchitem");
+const passport = require("passport");
+const config = require("../config");
 const moment = require("moment");
 
 // **********************************************************
-// Send stock info  ======================================
+// Send 5 positions of the user info  =======================
 // **********************************************************
+
+watchItemsController.get(
+  "/user",
+  passport.authenticate("jwt", config.jwtSession),
+  function(req, res, next) {
+    const user = req.user;
+
+    WatchItem.find({
+      userId: user._id,
+      position: { $in: ["bull", "bear"] }
+    })
+      .sort({ created_at: -1 })
+      .limit(5)
+      .populate("stockId")
+      .exec((err, watchitem) => {
+        console.log("watchitem*********", watchitem);
+        if (!watchitem) {
+          res.json(null);
+        } else {
+          res.json(watchitem);
+        }
+      });
+  }
+);
 
 module.exports = watchItemsController;
