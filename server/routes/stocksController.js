6827dedc8aa8f4e08bd197cdcaea93b8843d2764
Fred Magnin <frederic.magnin@gmail.com>
2017-10-26 19:35:01 +0200

    m

--- server/routes/stocksController.js
+++ server/routes/stocksController.js
@@ -13,13 +13,13 @@ const moment = require("moment");
 // **********************************************************
 
 stocksController.get("/:stockName", function(req, res, next) {
-  const stock = req.params.stockName.toUpperCase();
+    const stock = req.params.stockName.toUpperCase();
 
-  Stock.findOne({ longName: stock }, (err, stock) => {
-    if (err) return next(err);
-    if (!stock) return next(err);
-    res.json(stock);
-  });
+    Stock.findOne({ longName: stock }, (err, stock) => {
+        if (err) return next(err);
+        if (!stock) return next(err);
+        res.json(stock);
+    });
 });
 
 // **********************************************************
@@ -27,7 +27,6 @@ stocksController.get("/:stockName", function(req, res, next) {
 // **********************************************************
 
 stocksController.get("/:stockName/bull-bear-trend", function(req, res, next) {
-<<<<<<< HEAD
     let attribute = req.query.history;
     const stock = req.params.stockName.toUpperCase();
 
@@ -62,7 +61,6 @@ stocksController.get("/:stockName/bull-bear-trend", function(req, res, next) {
         });
     });
 });
-
 // **********************************************************
 // Send wachtlist info  =====================================
 // **********************************************************
@@ -152,133 +150,6 @@ stocksController.post(
             })
             .catch(err => console.error(err));
     }
-=======
-  let attribute = req.query.history;
-  const stock = req.params.stockName.toUpperCase();
-
-  Stock.findOne({ longName: stock }, (err, stock) => {
-    if (err) return next(err);
-    if (!stock) return next(err);
-
-    const today = moment().startOf("day");
-    const thirtyDaysAgo = moment(today).subtract(attribute, "days");
-
-    WatchItem.find({
-      stockId: stock._id,
-      status: "active",
-      created_at: {
-        $gte: thirtyDaysAgo.toDate()
-      }
-    }).then(activeWatchItems => {
-      function countPositions(array, position) {
-        return array
-          .map(item => {
-            return item.position == position;
-          })
-          .filter(val => {
-            return val === true;
-          }).length;
-      }
-      var nbBull = countPositions(activeWatchItems, "bull");
-      var nbBear = countPositions(activeWatchItems, "bear");
-      var percentage = [nbBull / (nbBull + nbBear), nbBear / (nbBull + nbBear)];
-
-      res.json(percentage);
-    });
-  });
-});
-// **********************************************************
-// Send wachtlist info  =====================================
-// **********************************************************
-
-stocksController.get(
-  "/:stockName/watchitem",
-  passport.authenticate("jwt", config.jwtSession),
-  function(req, res, next) {
-    const stock = req.params.stockName.toUpperCase();
-    const user = req.user;
-
-    Stock.findOne({ longName: stock }, (err, stock) => {
-      WatchItem.findOne({
-        userId: user._id,
-        stockId: stock._id,
-        status: "active"
-      }).then(watchitem => {
-        if (!watchitem) {
-          //const err = new Error("Not Found");
-          //err.status = 404;
-          res.json(null);
-        } else {
-          res.json(watchitem);
-        }
-      });
-    });
-  }
-);
-// ***************************************************
-// Send babbles info linked to a stock ===============
-// ***************************************************
-
-stocksController.get("/babbles", function(req, res, next) {
-  const stock = req.params.name.toUpperCase();
-
-  Stock.findOne({ longName: stock }, (err, stock) => {
-    if (err) return next(err);
-    if (!stock) return next(err);
-
-    Babble.find({ stockLink: stock._id })
-      .sort({ updated_at: -1 })
-      .populate("user")
-      .exec((err, timeline) => {
-        res.json({
-          timeline,
-          moment
-        });
-      });
-  });
-});
-
-// ***************************************************
-// Add stock to  watchlist ===========================
-// ***************************************************
-stocksController.post(
-  "/:name/watchitem/add",
-  passport.authenticate("jwt", config.jwtSession),
-  (req, res, next) => {
-    const user = req.user;
-    const stockName = req.params.name.toUpperCase();
-
-    Stock.findOne({ longName: stockName })
-      .then(stock => {
-        if (!stock) {
-          res.json({
-            error: req.app.get("env") === "development" ? err : {}
-          });
-        }
-        const newWatchItem = new WatchItem({
-          userId: user._id,
-          username: user.username,
-          stockId: stock._id,
-          position: "none"
-        });
-
-        newWatchItem.save().then(newItem => {
-          User.findByIdAndUpdate(
-            user._id,
-            {
-              $addToSet: { watchList: newItem._id }
-            },
-            { new: true }
-          ).then(user => {
-            req.user = user;
-            res.locals.user = user;
-            res.json(newItem);
-          });
-        });
-      })
-      .catch(err => console.error(err));
-  }
->>>>>>> fb1a8a4b9479f2f48ca5625211c2017dfcb97216
 );
 
 // ***************************************************
@@ -286,12 +157,11 @@ stocksController.post(
 // ***************************************************
 
 stocksController.delete(
-<<<<<<< HEAD
-    "/:name/watchitem",
+    "/:name/watchitem/:watchitemId",
     passport.authenticate("jwt", config.jwtSession),
     (req, res, next) => {
         const user = req.user;
-        const watchItemId = req.body.watchItemId;
+        const watchItemId = req.params.watchitemId;
         const stockName = req.params.name.toUpperCase();
 
         WatchItem.findByIdAndUpdate(watchItemId, { status: "removed" })
@@ -306,34 +176,12 @@ stocksController.delete(
                 console.log(err);
             });
     }
-=======
-  "/:name/watchitem/:watchitemId",
-  passport.authenticate("jwt", config.jwtSession),
-  (req, res, next) => {
-    const user = req.user;
-    const watchItemId = req.params.watchitemId;
-    const stockName = req.params.name.toUpperCase();
-
-    WatchItem.findByIdAndUpdate(watchItemId, { status: "removed" })
-      .then(resp => {
-        User.findByIdAndUpdate(user._id, {
-          $pull: { watchList: watchItemId }
-        }).then(resp => {
-          res.json({ success: true });
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
->>>>>>> fb1a8a4b9479f2f48ca5625211c2017dfcb97216
 );
 
 // ***************************************************
 // Take position BULL ===========================
 // ***************************************************
 stocksController.post(
-<<<<<<< HEAD
     "/:name/watchitem/bull",
     passport.authenticate("jwt", config.jwtSession),
     (req, res, next) => {
@@ -363,7 +211,7 @@ stocksController.post(
                     ).then(user => {
                         req.user = user;
                         res.locals.user = user;
-                        res.json({ success: true, watchItemId: newItem._id });
+                        res.json(newItem);
                     });
                 });
             })
@@ -372,28 +220,44 @@ stocksController.post(
 );
 
 // ***************************************************
-// Close position Bull =======================
+// Close position Bull & Bear =======================
 // ***************************************************
 
-stocksController.delete(
-    "/:name/watchitem/bull-rem",
+stocksController.patch(
+    "/:name/watchitem/:watchitemId",
     passport.authenticate("jwt", config.jwtSession),
     (req, res, next) => {
         const user = req.user;
-        const watchItemId = req.body.watchItemId;
+        const watchItemId = req.params.watchitemId;
         const stockName = req.params.name.toUpperCase();
 
         WatchItem.findById(watchItemId)
             .populate("stockId")
             .exec((err, watchItem) => {
                 console.log("************WATCHITEM*****", watchItem);
-                let newStatus =
-                    watchItem.stockId.price < watchItem.initialPrice ? "lost" : "won";
+                let position = watchItem.position;
+                if (position === "bull") {
+                    // Define new status
+                    let newStatus =
+                        watchItem.stockId.price < watchItem.initialPrice ? "lost" : "won";
 
-                // Update score of the user
-                let updateScore = Mathfloor(
-                    (watchItem.stockId.price - watchItem.initialPrice) * 10
-                );
+                    // Update score of the user
+                    const updateScore = Math.floor(
+                        (watchItem.stockId.price - watchItem.initialPrice) * 10
+                    );
+                    User.findByIdAndUpdate(user._id, {
+                        $inc: { score: updateScore }
+                    }).exec();
+                } else {
+                    let newStatus =
+                        watchItem.stockId.price > watchItem.initialPrice ? "lost" : "won";
+
+                    // Update score of the user
+                    const updateScore = Math.floor(
+                        (watchItem.stockId.price - watchItem.initialPrice) * -10
+                    );
+                }
+                // Update status of watchList
                 User.findByIdAndUpdate(user._id, {
                     $inc: { score: updateScore }
                 }).exec();
@@ -411,110 +275,12 @@ stocksController.delete(
                     });
             });
     }
-=======
-  "/:name/watchitem/bull",
-  passport.authenticate("jwt", config.jwtSession),
-  (req, res, next) => {
-    const user = req.user;
-    const stockName = req.params.name.toUpperCase();
-
-    Stock.findOne({ longName: stockName })
-      .then(stock => {
-        if (!stock) {
-          res.json({
-            error: req.app.get("env") === "development" ? err : {}
-          });
-        }
-        const newWatchItem = new WatchItem({
-          userId: user._id,
-          username: user.username,
-          stockId: stock._id,
-          initialPrice: stock.price,
-          position: "bull"
-        });
-
-        newWatchItem.save().then(newItem => {
-          User.findByIdAndUpdate(
-            user._id,
-            {
-              $addToSet: { watchList: newItem._id }
-            },
-            { new: true }
-          ).then(user => {
-            req.user = user;
-            res.locals.user = user;
-            res.json(newItem);
-          });
-        });
-      })
-      .catch(err => res.status(404));
-  }
-);
-
-// ***************************************************
-// Close position Bull & Bear =======================
-// ***************************************************
-
-stocksController.patch(
-  "/:name/watchitem/:watchitemId",
-  passport.authenticate("jwt", config.jwtSession),
-  (req, res, next) => {
-    const user = req.user;
-    const watchItemId = req.params.watchitemId;
-    const stockName = req.params.name.toUpperCase();
-
-    WatchItem.findById(watchItemId)
-      .populate("stockId")
-      .exec((err, watchItem) => {
-        console.log("************WATCHITEM*****", watchItem);
-        let position = watchItem.position;
-        if (position === "bull") {
-          // Define new status
-          let newStatus =
-            watchItem.stockId.price < watchItem.initialPrice ? "lost" : "won";
-
-          // Update score of the user
-          const updateScore = Math.floor(
-            (watchItem.stockId.price - watchItem.initialPrice) * 10
-          );
-          User.findByIdAndUpdate(user._id, {
-            $inc: { score: updateScore }
-          }).exec();
-        } else {
-          let newStatus =
-            watchItem.stockId.price > watchItem.initialPrice ? "lost" : "won";
-
-          // Update score of the user
-          const updateScore = Math.floor(
-            (watchItem.stockId.price - watchItem.initialPrice) * -10
-          );
-        }
-        // Update status of watchList
-        User.findByIdAndUpdate(user._id, {
-          $inc: { score: updateScore }
-        }).exec();
-
-        WatchItem.findByIdAndUpdate(watchItemId, { status: newStatus })
-          .then(resp => {
-            User.findByIdAndUpdate(user._id, {
-              $pull: { watchList: watchItemId }
-            }).then(resp => {
-              res.json({ success: true });
-            });
-          })
-          .catch(err => {
-            console.log(err);
-          });
-      });
-  }
->>>>>>> fb1a8a4b9479f2f48ca5625211c2017dfcb97216
 );
 
 // ***************************************************
 // Take position BEAR ================================
 // ***************************************************
 stocksController.post(
-<<<<<<< HEAD
     "/:name/watchitem/bear",
     passport.authenticate("jwt", config.jwtSession),
     (req, res, next) => {
@@ -555,90 +321,4 @@ stocksController.post(
     }
 );
 
-// ***************************************************
-// Close position Bear =======================
-// ***************************************************
-
-stocksController.delete(
-    "/:name/watchitem/bear-rem",
-    passport.authenticate("jwt", config.jwtSession),
-    (req, res, next) => {
-        const user = req.user;
-        const watchItemId = req.body.watchItemId;
-        const stockName = req.params.name.toUpperCase();
-
-        WatchItem.findById(watchItemId)
-            .populate("stockId")
-            .then(watchItem => {
-                let newStatus =
-                    watchItem.stockId.price > watchItem.initialPrice ? "lost" : "won";
-
-                // Update score of the user
-                let updateScore = Mathfloor(
-                    (watchItem.stockId.price - watchItem.initialPrice) * -1000
-                );
-                User.findByIdAndUpdate(user._id, {
-                    $inc: { score: updateScore }
-                }).exec();
-
-                WatchItem.findByIdAndUpdate(watchItemId, { status: newStatus })
-                    .then(resp => {
-                        User.findByIdAndUpdate(user._id, {
-                            $pull: { watchList: watchItemId }
-                        }).then(resp => {
-                            res.json({ success: true });
-                        });
-                    })
-                    .catch(err => {
-                        console.log(err);
-                    });
-            });
-    }
-);
-
 module.exports = stocksController;
-=======
-  "/:name/watchitem/bear",
-  passport.authenticate("jwt", config.jwtSession),
-  (req, res, next) => {
-    const user = req.user;
-    const stockName = req.params.name.toUpperCase();
-
-    Stock.findOne({ longName: stockName })
-      .then(stock => {
-        if (!stock) {
-          res.json({
-            error: req.app.get("env") === "development" ? err : {}
-          });
-        }
-        const newWatchItem = new WatchItem({
-          userId: user._id,
-          username: user.username,
-          stockId: stock._id,
-          initialPrice: stock.price,
-          position: "bear"
-        });
-
-        newWatchItem.save().then(newItem => {
-          User.findByIdAndUpdate(
-            user._id,
-            {
-              $addToSet: { watchList: newItem._id }
-            },
-            { new: true }
-          ).then(user => {
-            req.user = user;
-            res.locals.user = user;
-            res.json({
-              success: true,
-              watchItemId: newItem._id
-            });
-          });
-        });
-      })
-      .catch(err => res.status(404));
-  }
-);
-
-module.exports = stocksController;
->>>>>>> fb1a8a4b9479f2f48ca5625211c2017dfcb97216
