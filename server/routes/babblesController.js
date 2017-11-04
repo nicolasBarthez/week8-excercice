f0ce61bf6c55af2470331761568de7e7a52e881f
Nicolas <nicolas.barthez@gmail.com>
2017-11-04 19:08:42 +0100

    fixin babble sort issues

--- server/routes/babblesController.js
+++ server/routes/babblesController.js
@@ -16,58 +16,65 @@ const moment = require("moment");
 // ***************************************************
 
 babblesController.get(
-    "/",
-    passport.authenticate("jwt", config.jwtSession),
-    function(req, res, next) {
-        const user = req.user;
-        // sort by people who wrote the babble
-        const sort = req.query.sort;
-        console.log(sort)
-        const page = req.query.page;
-        const group = page * 50;
+  "/",
+  passport.authenticate("jwt", config.jwtSession),
+  function(req, res, next) {
+    const user = req.user;
+    // sort by people who wrote the babble
+    const sort = req.query.sort;
+    console.log(sort);
+    const page = req.query.page;
+    const group = page * 50;
 
-        if (sort === "all") {
-            Babble.find()
-                .sort({ updated_at: -1 })
-                .populate("user")
-                .limit(group)
-                .exec((err, timeline) => {
-                    if (err) return res.json(null);
-                    res.json(timeline);
-                });
-        } else if (sort === "me") {
-            Babble.find({ user: user._id })
-                .sort({ updated_at: -1 })
-                .populate("user")
-                .limit(group)
-                .exec((err, timeline) => {
-                    if (err) return res.json(null);
-                    res.json(timeline);
-                });
-        } else if (sort === "insidermates") {
-            User.findById(user._id).then(us => {
-                Babble.find({ $in: { user: us.following } })
-                    .sort({ updated_at: -1 })
-                    .populate("user")
-                    .limit(group)
-                    .exec((err, timeline) => {
-                        if (err) return res.json(null);
-                        res.json(timeline);
-                    });
+    if (sort === "all") {
+      Babble.find()
+        .sort({ updated_at: -1 })
+        .populate("user")
+        .limit(group)
+        .exec((err, timeline) => {
+          if (err) return res.json(null);
+          res.json(timeline);
+        });
+    } else if (sort === "me") {
+      Babble.find({ user: user._id })
+        .sort({ updated_at: -1 })
+        .populate("user")
+        .limit(group)
+        .exec((err, timeline) => {
+          if (err) return res.json(null);
+          res.json(timeline);
+        });
+    } else if (sort === "insidermates") {
+      User.findById(user._id).then(us => {
+        Babble.find({ user: { $in: us.following } })
+          .sort({ updated_at: -1 })
+          .populate("user")
+          .limit(group)
+          .exec((err, timeline) => {
+            if (err) console.log("ERROR", err);
+
+            if (err) return res.json(null);
+            res.json(timeline);
+          });
+      });
+    } else if (sort === "watchlist") {
+      User.findById(user._id)
+        .populate("watchList")
+        .exec((err, us) => {
+          let stockArray = us.watchList.map(el => {
+            return el.stockId;
+          });
+          Babble.find({ stockLink: { $in: stockArray } })
+            .sort({ updated_at: -1 })
+            .populate("user")
+            .limit(group)
+            .exec((err, timeline) => {
+              if (err) return res.json(null);
+              res.json(timeline);
             });
-        } else if (sort === "watchlist") {
-            User.findById(user._id).then(us => {
-                Babble.find({ $in: { user: us.following } })
-                    .sort({ updated_at: -1 })
-                    .populate("user")
-                    .limit(group)
-                    .exec((err, timeline) => {
-                        if (err) return res.json(null);
-                        res.json(timeline);
-                    });
-            });
-        }
+        });
     }
+  }
 );
 
 // ***************************************************
@@ -75,64 +82,62 @@ babblesController.get(
 // ***************************************************
 
 babblesController.get(
-    "/:name",
-    passport.authenticate("jwt", config.jwtSession),
-    function(req, res, next) {
-        const stock = req.params.name.toUpperCase();
-        const user = req.user;
-        // sort by people who wrote the babble
-        const sort = req.query.sort;
-        const page = req.query.page;
-        const group = page * 50;
+  "/:name",
+  passport.authenticate("jwt", config.jwtSession),
+  function(req, res, next) {
+    const stock = req.params.name.toUpperCase();
+    const user = req.user;
+    // sort by people who wrote the babble
+    const sort = req.query.sort;
+    const page = req.query.page;
+    const group = page * 50;
 
-        if (sort === "all") {
-            Stock.findOne({ longName: stock }, (err, stock) => {
-                if (err) return next(err);
-                if (!stock) return next(err);
+    if (sort === "all") {
+      Stock.findOne({ longName: stock }, (err, stock) => {
+        if (err) return next(err);
+        if (!stock) return next(err);
 
-                Babble.find({ stockLink: stock._id })
-                    .sort({ updated_at: -1 })
-                    .populate("user")
-                    .limit(group)
-                    .exec((err, timeline) => {
-                        if (err) return res.json(null);
-                        res.json(timeline);
-                    });
+        Babble.find({ stockLink: stock._id })
+          .sort({ updated_at: -1 })
+          .populate("user")
+          .limit(group)
+          .exec((err, timeline) => {
+            if (err) return res.json(null);
+            res.json(timeline);
+          });
+      });
+    } else if (sort === "me") {
+      Stock.findOne({ longName: stock }, (err, stock) => {
+        if (err) return next(err);
+        if (!stock) return next(err);
+
+        Babble.find({ stockLink: stock._id, user: user._id })
+          .sort({ updated_at: -1 })
+          .populate("user")
+          .limit(group)
+          .exec((err, timeline) => {
+            if (err) return res.json(null);
+            res.json(timeline);
+          });
+      });
+    } else if (sort === "insidermates") {
+      Stock.findOne({ longName: stock }, (err, stock) => {
+        if (err) return next(err);
+        if (!stock) return next(err);
+
+        User.findById(user._id).then(us => {
+          Babble.find({ stockLink: stock._id, user: { $in: us.following } })
+            .sort({ updated_at: -1 })
+            .populate("user")
+            .limit(group)
+            .exec((err, timeline) => {
+              if (err) return res.json(null);
+              res.json(timeline);
             });
-        } else if (sort === "me") {
-            Stock.findOne({ longName: stock }, (err, stock) => {
-                if (err) return next(err);
-                if (!stock) return next(err);
-
-                Babble.find({ stockLink: stock._id, user: user._id })
-                    .sort({ updated_at: -1 })
-                    .populate("user")
-                    .limit(group)
-                    .exec((err, timeline) => {
-                        if (err) return res.json(null);
-                        res.json(timeline);
-                    });
-            });
-        } else if (sort === "insidermates") {
-            Stock.findOne({ longName: stock }, (err, stock) => {
-                if (err) return next(err);
-                if (!stock) return next(err);
-
-                User.findById(user._id).then(us => {
-                    Babble.find({ stockLink: stock._id, $in: { user: us.following } })
-                        .sort({ updated_at: -1 })
-                        .populate("user")
-                        .limit(group)
-                        .exec((err, timeline) => {
-                            if (err) return res.json(null);
-                            res.json(timeline);
-                        });
-
-                });
-            });
-
-        }
+        });
+      });
     }
+  }
 );
 
 // **********************************************************
@@ -140,30 +145,30 @@ babblesController.get(
 // **********************************************************
 
 babblesController.post(
-    "/",
-    passport.authenticate("jwt", config.jwtSession),
-    function(req, res, next) {
-        const stock = req.query.stock ? req.query.stock : "";
-        const babble = req.body.babble;
-        const user = req.user;
+  "/",
+  passport.authenticate("jwt", config.jwtSession),
+  function(req, res, next) {
+    const stock = req.query.stock ? req.query.stock : "";
+    const babble = req.body.babble;
+    const user = req.user;
 
-        const newBabble = new Babble({
-            user: user._id,
-            babble: babble,
-            stockLink: stock
-        });
+    const newBabble = new Babble({
+      user: user._id,
+      babble: babble,
+      stockLink: stock
+    });
 
-        newBabble.save(err => {
-            if (err) {
-                res.json(err);
-            } else {
-                // GAMIFICATION => +20 points per babble  posted
-                User.findByIdAndUpdate(user._id, { $inc: { score: 20 } }).exec();
+    newBabble.save(err => {
+      if (err) {
+        res.json(err);
+      } else {
+        // GAMIFICATION => +20 points per babble  posted
+        User.findByIdAndUpdate(user._id, { $inc: { score: 20 } }).exec();
 
-                res.json(newBabble);
-            }
-        });
-    }
+        res.json(newBabble);
+      }
+    });
+  }
 );
 
 // **********************************************************
@@ -171,57 +176,57 @@ babblesController.post(
 // **********************************************************
 
 babblesController.post(
-    "/resp",
-    passport.authenticate("jwt", config.jwtSession),
-    function(req, res, next) {
-        const stock = req.query.stockId ? req.query.stockId : "";
-        const babbleResp = req.body.babble;
-        const parentBabbleId = req.query.respto;
-        const user = req.user;
+  "/resp",
+  passport.authenticate("jwt", config.jwtSession),
+  function(req, res, next) {
+    const stock = req.query.stockId ? req.query.stockId : "";
+    const babbleResp = req.body.babble;
+    const parentBabbleId = req.query.respto;
+    const user = req.user;
 
-        const newReply = new Babblereply({
-            user: user._id,
-            username: user.username,
-            picProfile: user.picProfile,
-            babble: babbleResp
-        });
+    const newReply = new Babblereply({
+      user: user._id,
+      username: user.username,
+      picProfile: user.picProfile,
+      babble: babbleResp
+    });
 
-        // GAMIFICATION => +10 points per babble replied posted
-        User.findByIdAndUpdate(user._id, { $inc: { score: 10 } }).exec();
+    // GAMIFICATION => +10 points per babble replied posted
+    User.findByIdAndUpdate(user._id, { $inc: { score: 10 } }).exec();
 
-        Babble.findByIdAndUpdate(parentBabbleId, {
-            $push: { reply: newReply }
-        }).then(resp => {
-            return res.json(newReply);
-        });
-    }
+    Babble.findByIdAndUpdate(parentBabbleId, {
+      $push: { reply: newReply }
+    }).then(resp => {
+      return res.json(newReply);
+    });
+  }
 );
 
 // **********************************************************
 // Like a babble  (only once) ===============================
 // **********************************************************
 babblesController.post(
-    "/like",
-    passport.authenticate("jwt", config.jwtSession),
-    (req, res, next) => {
-        const babble = req.body.babbleId;
-        const user = req.user;
+  "/like",
+  passport.authenticate("jwt", config.jwtSession),
+  (req, res, next) => {
+    const babble = req.body.babbleId;
+    const user = req.user;
 
-        Babble.findByIdAndUpdate(babble, {
-            $addToSet: { like: user._id }
-        }).exec();
-        Babble.findByIdAndUpdate(babble)
-            .populate("user")
-            .exec((err, whoIsLiked) => {
-                if (err) return next(err);
-                // GAMIFICATION => receive 10 points because receive 1 like
-                User.findByIdAndUpdate(whoIsLiked._id, {
-                    $inc: { score: 10 }
-                }).exec((err, executed) => {
-                    if (err) return next(err);
-                });
-                res.json(babble);
-            });
-    }
+    Babble.findByIdAndUpdate(babble, {
+      $addToSet: { like: user._id }
+    }).exec();
+    Babble.findByIdAndUpdate(babble)
+      .populate("user")
+      .exec((err, whoIsLiked) => {
+        if (err) return next(err);
+        // GAMIFICATION => receive 10 points because receive 1 like
+        User.findByIdAndUpdate(whoIsLiked._id, {
+          $inc: { score: 10 }
+        }).exec((err, executed) => {
+          if (err) return next(err);
+        });
+        res.json(babble);
+      });
+  }
 );
 module.exports = babblesController;
