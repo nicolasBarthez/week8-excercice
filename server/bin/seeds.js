ee3f89fc9233503badb3f7b8b9aec1a6eea321ca
Nicolas <nicolas.barthez@gmail.com>
2017-11-05 19:28:08 +0100

    insert nasdaq + update share stock

--- server/bin/seeds.js
+++ server/bin/seeds.js
@@ -5,13 +5,22 @@ const mongoose = require("mongoose");
 // const getstockUpdate = require("./getstocksPrice");
 mongoose.connect(process.env.MONGODB_URI);
 
-const StockData = require("../data/cac40");
+const cac40 = require("../data/cac40");
+const nasdaq = require("../data/nasdaq");
 
 // Remove all Stock data
 Stock.collection.drop();
 
 // Create stock data
-Stock.create(StockData, (err, stock) => {
+Stock.create(nasdaq, (err, stock) => {
+  if (err) {
+    console.error(err);
+  }
+  stock.forEach(stock => console.log(stock.longName));
+  mongoose.connection.close();
+});
+
+Stock.create(cac40, (err, stock) => {
   if (err) {
     console.error(err);
   }
